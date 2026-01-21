f5a167810b3648b02ec3d068749b73f1dc08940b
Steveh4wk <66309278+Steveh4wk@users.noreply.github.com>
2026-01-21 08:24:20 +0100

    runa sys extern file

---
+++ ox_inventory/data/items.lua
@@ -0,0 +1,362 @@
+return {
+	['runa_hp'] = {
+		label = 'Runa HP',
+		weight = 100,
+		stack = true,
+		close = true,
+		description = 'Aumenta la salute massima',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_danno'] = {
+		label = 'Runa Danno',
+		weight = 100,
+		stack = true,
+		close = true,
+		description = 'Aumenta il danno delle armi',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	['runa_mp'] = {
+		label = 'Runa MP',
+		weight = 100,
+		stack = true,
+		close = true,
+		description = 'Aumenta i punti mana',
+		client = {
+			image = 'runa_mp.png'
+		}
+	},
+
+	['runa_cdr'] = {
+		label = 'Runa CDR',
+		weight = 100,
+		stack = true,
+		close = true,
+		description = 'Riduce il cooldown delle abilità',
+		client = {
+			image = 'runa_cdr.png'
+		}
+	},
+
+	['runa_speed'] = {
+		label = 'Runa Speed',
+		weight = 100,
+		stack = true,
+		close = true,
+		description = 'Aumenta la velocità di movimento',
+		client = {
+			image = 'runa_speed.png'
+		}
+	},
+
+	['pietra_grezza'] = {
+		label = 'Pietra Grezza',
+		weight = 500,
+		stack = true,
+		description = 'Pietra utilizzata per craftare rune'
+	},
+
+	['runa_fortuna'] = {
+		label = 'Runa Fortuna',
+		weight = 100,
+		stack = true,
+		close = true,
+		description = 'Aumenta la fortuna e i drop rate',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_mistica'] = {
+		label = 'Runa Mistica',
+		weight = 150,
+		stack = true,
+		close = true,
+		description = 'Rara runa con poteri mistici',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	-- Rune con estensioni per upgrade system
+	['runa_hp_1'] = {
+		label = 'Runa HP +1',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa HP migliorata che aumenta la salute massima del 10%',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_hp_2'] = {
+		label = 'Runa HP +2',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa HP avanzata che aumenta la salute massima del 25%',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_hp_3'] = {
+		label = 'Runa HP +3',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa HP superiore che aumenta la salute massima del 50%',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_hp_4'] = {
+		label = 'Runa HP +4',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa HP epica che aumenta la salute massima del 75%',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_danno_1'] = {
+		label = 'Runa Danno +1',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Danno migliorata che aumenta il danno del 15%',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	['runa_danno_2'] = {
+		label = 'Runa Danno +2',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Danno avanzata che aumenta il danno del 30%',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	['runa_danno_3'] = {
+		label = 'Runa Danno +3',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Danno superiore che aumenta il danno del 50%',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	['runa_danno_4'] = {
+		label = 'Runa Danno +4',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Danno epica che aumenta il danno del 75%',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	['runa_mp_1'] = {
+		label = 'Runa MP +1',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa MP migliorata che aumenta il mana massimo del 20%',
+		client = {
+			image = 'runa_mp.png'
+		}
+	},
+
+	['runa_mp_2'] = {
+		label = 'Runa MP +2',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa MP avanzata che aumenta il mana massimo del 40%',
+		client = {
+			image = 'runa_mp.png'
+		}
+	},
+
+	['runa_mp_3'] = {
+		label = 'Runa MP +3',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa MP superiore che aumenta il mana massimo del 60%',
+		client = {
+			image = 'runa_mp.png'
+		}
+	},
+
+	['runa_mp_4'] = {
+		label = 'Runa MP +4',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa MP epica che aumenta il mana massimo dell\'80%',
+		client = {
+			image = 'runa_mp.png'
+		}
+	},
+
+	['runa_cdr_1'] = {
+		label = 'Runa CDR +1',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa CDR migliorata che riduce i cooldown del 10%',
+		client = {
+			image = 'runa_cdr.png'
+		}
+	},
+
+	['runa_cdr_2'] = {
+		label = 'Runa CDR +2',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa CDR avanzata che riduce i cooldown del 25%',
+		client = {
+			image = 'runa_cdr.png'
+		}
+	},
+
+	['runa_cdr_3'] = {
+		label = 'Runa CDR +3',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa CDR superiore che riduce i cooldown del 40%',
+		client = {
+			image = 'runa_cdr.png'
+		}
+	},
+
+	['runa_cdr_4'] = {
+		label = 'Runa CDR +4',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa CDR epica che riduce i cooldown del 60%',
+		client = {
+			image = 'runa_cdr.png'
+		}
+	},
+
+	['runa_speed_1'] = {
+		label = 'Runa Speed +1',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Speed migliorata che aumenta la velocità del 10%',
+		client = {
+			image = 'runa_speed.png'
+		}
+	},
+
+	['runa_speed_2'] = {
+		label = 'Runa Speed +2',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Speed avanzata che aumenta la velocità del 20%',
+		client = {
+			image = 'runa_speed.png'
+		}
+	},
+
+	['runa_speed_3'] = {
+		label = 'Runa Speed +3',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Speed superiore che aumenta la velocità del 30%',
+		client = {
+			image = 'runa_speed.png'
+		}
+	},
+
+	['runa_speed_4'] = {
+		label = 'Runa Speed +4',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa Speed epica che aumenta la velocità del 40%',
+		client = {
+			image = 'runa_speed.png'
+		}
+	},
+
+	-- Rune divine +5 (trasformazione finale)
+	['runa_hp_divina'] = {
+		label = 'Runa HP Divina +5',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa massimizzata che conferisce immortalità temporanea e rigenerazione istantanea',
+		client = {
+			image = 'runa_hp.png'
+		}
+	},
+
+	['runa_danno_divina'] = {
+		label = 'Runa Danno Divina +5',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa massimizzata che triplica il danno e aggiunge effetti elementali',
+		client = {
+			image = 'runa_danno.png'
+		}
+	},
+
+	['runa_mp_divina'] = {
+		label = 'Runa MP Divina +5',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa massimizzata che garantisce mana infinito e cast istantanei',
+		client = {
+			image = 'runa_mp.png'
+		}
+	},
+
+	['runa_cdr_divina'] = {
+		label = 'Runa CDR Divina +5',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa massimizzata che elimina tutti i cooldown e permette abilità concatenate',
+		client = {
+			image = 'runa_cdr.png'
+		}
+	},
+
+	['runa_speed_divina'] = {
+		label = 'Runa Speed Divina +5',
+		weight = 100,
+		stack = false,
+		close = true,
+		description = 'Runa massimizzata che conferisce velocità suprema e teletrasporto breve',
+		client = {
+			image = 'runa_speed.png'
+		}
+	}
+}
